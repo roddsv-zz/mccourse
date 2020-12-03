84998b2089e83416350a65c7fc890aa851b6a398
Rodrigo Soares <adsrodsoares@gmail.com>
2020-12-03 15:46:45 -0300

    Created Project

---
+++ src/main/java/com/rodrigo/mccourse/MccourseApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.mccourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MccourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MccourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigo/mccourse/MccourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.mccourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
