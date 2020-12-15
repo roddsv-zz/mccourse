fb719e56af732d4679375379926a5b4d320f2a50
Rodrigo Soares <adsrodsoares@gmail.com>
2020-12-14 21:26:13 -0300

    Creating Instantiation Operations

--- src/main/java/com/rodrigo/mccourse/MccourseApplication.java
+++ src/main/java/com/rodrigo/mccourse/MccourseApplication.java
@@ -1,13 +1,35 @@
 package com.rodrigo.mccourse;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rodrigo.mccourse.domain.Category;
+import com.rodrigo.mccourse.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class MccourseApplication {
+public class MccourseApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MccourseApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
+		
 	}
 
 }
